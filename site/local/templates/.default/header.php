c700dd8bfa8e9519bcbf92cdceee01f2c5d75a19
sevastyan <seva@aaccent.ru>
2024-06-04 17:55:19 +0300

    init commit

---
+++ site/local/templates/.default/header.php
@@ -0,0 +1,10 @@
+<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Сайт</title>
+</head>
+<body>
+
+<?php $APPLICATION->ShowPanel();?>
